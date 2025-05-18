3a3005e7491acaf4e84f386b4d1fc6340b8c4fc6
euch <euch@mail>
2025-05-18 12:11:41 +0400

    fdwh_ai_descr_helper integration

--- fdwh_dags_core_etl/dwh_refresh.py
+++ fdwh_dags_core_etl/dwh_refresh.py
@@ -1,16 +1,14 @@
-from datetime import datetime
-
 from airflow import DAG
 from airflow.models import Variable
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.utils.task_group import TaskGroup
 
-from operators.add_metadata_operator import AddMetadataOperator
-from operators.bulk_insert_operator import BulkInsertOperator
-from operators.create_remote_tree_csv_operator import CreateRemoteTreeCsvOperator
-from operators.smb_download_operator import SmbDownloadOperator
-
 from fdwh_config import *
+from fdwh_operators.add_ai_descr_operator import AddAiDescrVitGpt2Operator
+from fdwh_operators.add_metadata_operator import AddMetadataOperator
+from fdwh_operators.bulk_insert_operator import BulkInsertOperator
+from fdwh_operators.create_remote_tree_csv_operator import CreateRemoteTreeCsvOperator
+from fdwh_operators.smb_download_operator import SmbDownloadOperator
 
 with (DAG(dag_id=DAG_NAME_DWH_REFRESH, max_active_runs=1, schedule=SCHEDULE_DAILY) as dag):
     def refresh_raw(smb_conn_name: str, tree_file_csv_rp: str, tree_file_csv_lp: str, pg_tree_table: str):
@@ -51,12 +49,12 @@ with (DAG(dag_id=DAG_NAME_DWH_REFRESH, max_active_runs=1, schedule=SCHEDULE_DAIL
                         pg_tree_table="raw.tree_trash")
 
     with TaskGroup(group_id='edm') as edm:
-        delete_old = SQLExecuteQueryOperator(
-            task_id='delete_old',
-            conn_id=CONN_POSTGRES,
-            sql='sql/edm/edm_tree_delete_old.sql'
-        )
-        with TaskGroup(group_id='add_new') as add_new_tg:
+        tree_delete_old = SQLExecuteQueryOperator(task_id='tree_delete_old', conn_id=CONN_POSTGRES,
+                                                  sql='sql/edm/edm_tree_delete_old.sql')
+        tree_insert_new = SQLExecuteQueryOperator(task_id='tree_insert_new', conn_id=CONN_POSTGRES,
+                                                  sql='sql/edm/edm_tree_insert_new.sql')
+
+        with TaskGroup(group_id='add_missing_metadata') as add_missing_metadata:
             def add_new(find_missing_metadata_sql: str, xcom_key: str, smb_conn_name: str, rp: str):
                 SQLExecuteQueryOperator(
                     task_id='find_missing',
@@ -71,29 +69,38 @@ with (DAG(dag_id=DAG_NAME_DWH_REFRESH, max_active_runs=1, schedule=SCHEDULE_DAIL
                     filename_converter_func=lambda abs_filename: abs_filename.replace(rp, ''))
 
 
-            with TaskGroup(group_id='metadata_archive') as add_metadata_archive:
+            with TaskGroup(group_id='archive') as add_metadata_archive:
                 add_new(find_missing_metadata_sql='sql/edm/find_missing_metadata_archive.sql',
-                        xcom_key='edm.add_new.metadata_archive.find_missing',
+                        xcom_key='edm.add_missing_metadata.archive.find_missing',
                         smb_conn_name=CONN_SMB_ARCHIVE,
                         rp=Variable.get(VAR_RP_ARCHIVE))
 
-            with TaskGroup(group_id='metadata_collection') as add_metadata_collection:
+            with TaskGroup(group_id='collection') as add_metadata_collection:
                 add_new(find_missing_metadata_sql='sql/edm/find_missing_metadata_collection.sql',
-                        xcom_key='edm.add_new.metadata_collection.find_missing',
+                        xcom_key='edm.add_missing_metadata.collection.find_missing',
                         smb_conn_name=CONN_SMB_COLLECTION,
                         rp=Variable.get(VAR_RP_COLLECTION))
 
-            with TaskGroup(group_id='metadata_trash') as add_metadata_trash:
+            with TaskGroup(group_id='trash') as add_metadata_trash:
                 add_new(find_missing_metadata_sql='sql/edm/find_missing_metadata_trash.sql',
-                        xcom_key='edm.add_new.metadata_trash.find_missing',
+                        xcom_key='edm.add_missing_metadata.trash.find_missing',
                         smb_conn_name=CONN_SMB_TRASH,
                         rp=Variable.get(VAR_RP_TRASH))
 
-            SQLExecuteQueryOperator(task_id='tree', conn_id=CONN_POSTGRES, sql='sql/edm/edm_tree_insert_new.sql') >> [
-                add_metadata_archive,
-                add_metadata_collection,
-                add_metadata_trash]
-        delete_old >> add_new_tg
+        with TaskGroup(group_id='add_missing_ai_descr') as add_missing_ai_descr:
+
+            with TaskGroup(group_id='collection') as add_ai_descr_collection:
+                SQLExecuteQueryOperator(
+                    task_id='find_missing',
+                    conn_id=CONN_POSTGRES,
+                    sql='sql/edm/find_missing_ai_descr_collection.sql'
+                ) >> AddAiDescrVitGpt2Operator(
+                    task_id='add_missing',
+                    xcom_key='edm.add_missing_ai_descr.collection.find_missing',
+                    pg_conn_name=CONN_POSTGRES,
+                    ai_descr_endpoint=Variable.get(VAR_AI_DESCR_ENDPOINT))
+
+        tree_delete_old >> tree_insert_new >> add_missing_metadata >> add_missing_ai_descr
 
     with TaskGroup(group_id='dm') as dm:
         SQLExecuteQueryOperator(
