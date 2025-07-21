25b8b36ab9afc3024c2623856cc6f0781fc6e452
Evgenii Chernyshov <Evgenii_Chernyshov@rntgroup.com>
2025-07-21 14:22:26 +0400

    CheckHelperAvailableOperator skip instead of fail on TimeoutError

--- fdwh_op_check_helper_available.py
+++ fdwh_op_check_helper_available.py
@@ -15,9 +15,13 @@ class CheckHelperAvailableOperator(BaseOperator):
         host = parsed_url.hostname
         port = parsed_url.port if parsed_url.port else (80 if parsed_url.scheme == 'http' else 443)
         try:
-            with socket.create_connection((host, port), timeout=60):
+            with socket.create_connection((host, port), timeout=5) as conn:
+                conn.close()
                 pass
-        except OSError as e:
+        except (OSError, TimeoutError) as e:
             if '[Errno 113]' in str(e):
                 raise AirflowSkipException("No route to host") from e
+            if 'timed out' in str(e):
+                raise AirflowSkipException("Host available, connection to port timed out") from e
             raise e
+
