8b42a5bfcee0ed219df04fa139fde1106517012f
euch <1@>
2025-11-30 23:21:11 +0400

    tree_rel_path_insert.sql added

--- fdwh_core_tree/dag_refresh_tree.py
+++ fdwh_core_tree/dag_refresh_tree.py
@@ -1,6 +1,6 @@
-from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator, BranchSQLOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.providers.standard.operators.empty import EmptyOperator
-from airflow.sdk import Asset, DAG
+from airflow.sdk import Asset, DAG, Variable
 
 from fdwh_config import *
 
@@ -9,9 +9,8 @@ tags = {
     DagTag.FDWH_CORE,
     DagTag.PG,
 }
-with DAG(dag_id=DagName.REFRESH_CORE_TREE, max_active_runs=1, schedule=schedule, default_args=dag_args_noretry,
-         tags=tags):
-
+with (DAG(dag_id=DagName.REFRESH_CORE_TREE, max_active_runs=1, schedule=schedule, default_args=dag_args_noretry,
+          tags=tags)):
     SQLExecuteQueryOperator(
         task_id='tree_delete_old',
         conn_id=Conn.POSTGRES,
@@ -20,4 +19,13 @@ with DAG(dag_id=DagName.REFRESH_CORE_TREE, max_active_runs=1, schedule=schedule,
         task_id='tree_insert_new',
         conn_id=Conn.POSTGRES,
         sql='sql/tree_insert_new.sql'
+    ) >> SQLExecuteQueryOperator(
+        task_id='tree_rel_path_insert',
+        conn_id=Conn.POSTGRES,
+        sql='sql/tree_rel_path_insert.sql',
+        parameters={
+            'pattern_archive': '^' + Variable.get(VariableName.STORAGE_PATH_ARCHIVE),
+            'pattern_collection': '^' + Variable.get(VariableName.STORAGE_PATH_COLLECTION),
+            'pattern_trash': '^' + Variable.get(VariableName.STORAGE_PATH_TRASH)
+        }
     ) >> EmptyOperator(task_id="finish", outlets=Asset(AssetName.CORE_TREE_UPDATED))
