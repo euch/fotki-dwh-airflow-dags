0d4e6ff1251788a3d484f598353f36c17acbc3d6
euch <1@>
2026-03-18 17:47:31 +0400

    further add_missing_caption optimisation: new schedule, conditional reschedule/asset creation, trash processing added

--- dags/dwh/core/dag_add_missing_caption.py
+++ dags/dwh/core/dag_add_missing_caption.py
@@ -1,22 +1,28 @@
 import base64
 import io
-from datetime import timedelta
+from enum import Enum
 
 import requests
 from airflow.exceptions import AirflowException
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.sdk import Asset, Variable, dag, task
-from airflow.timetables.trigger import DeltaTriggerTimetable
+from airflow.timetables.assets import AssetOrTimeSchedule
+from airflow.timetables.trigger import CronTriggerTimetable
 
 from config import *
 
-schedule = DeltaTriggerTimetable(timedelta(minutes=1))
+schedule = AssetOrTimeSchedule(
+    timetable=CronTriggerTimetable("0 23 * * *", timezone='UTC'),
+    assets=Asset(AssetName.CORE_METADATA_UPDATED))
 tags = {
     DagTag.DWH_CORE,
     DagTag.HELPERS,
     DagTag.PG,
     DagTag.SMB,
 }
+dag_name = DagName.ADD_MISSING_CAPTION
 
 _missing_caption_select_sql = '''
 select
@@ -65,7 +71,16 @@ where
 '''
 
 
-@dag(dag_id=DagName.ADD_MISSING_CAPTION, max_active_runs=1, default_args=dag_args_noretry,
+class CaptionStatus(str, Enum):
+    NO_RECORDS_FOUND = 'no_records_found'
+    HAS_MORE_RECORDS = 'has_more_records'
+    NO_RECORDS_LEFT = 'no_records_left'
+
+    def __str__(self):
+        return str(self.value)
+
+
+@dag(dag_id=dag_name, max_active_runs=1, default_args=dag_args_noretry,
      schedule=schedule, tags=tags)
 def dag():
     @task
@@ -86,38 +101,70 @@ def dag():
     def add_missing_caption_archive(settings: dict):
         return _add_missing_caption(settings, 'archive', 100)
 
-    @task(outlets=[Asset(AssetName.CORE_CAPTION_UPDATED)])
-    def end():
-        pass
+    @task()
+    def add_missing_caption_trash(settings: dict):
+        return _add_missing_caption(settings, 'trash', 100)
+
+    @task.short_circuit()
+    def has_more_records(results: list[CaptionStatus]) -> bool:
+        for item in results:
+            if item == CaptionStatus.HAS_MORE_RECORDS:
+                return True
+
+        return False
+
+    trigger_again = TriggerDagRunOperator(task_id='trigger_again', trigger_dag_id=dag_name)
+
+    @task.short_circuit()
+    def some_records_processed(results: list[CaptionStatus]):
+        for item in results:
+            if item == CaptionStatus.HAS_MORE_RECORDS or item == CaptionStatus.NO_RECORDS_LEFT:
+                return True
+
+        return False
+
+    create_asset = EmptyOperator(task_id='create_asset', outlets=[Asset(AssetName.CORE_CAPTION_UPDATED)])
 
     caption_conf = get_caption_conf()
+
     process_collection = add_missing_caption_collection(caption_conf)
     process_archive = add_missing_caption_archive(caption_conf)
-    process_collection >> process_archive >> end()
+    process_trash = add_missing_caption_trash(caption_conf)
+
+    process_collection >> process_archive >> process_trash
+
+    has_more_records([process_collection, process_archive, process_trash]) >> trigger_again
+
+    some_records_processed([process_collection]) >> create_asset
 
 
 dag()
 
 
-def _add_missing_caption(caption_conf: dict, tree_type: str, limit: int) -> list[str]:
-    processed_abs_filenames = []
+def _add_missing_caption(caption_conf: dict, tree_type: str, limit: int) -> CaptionStatus:
     pg_hook: PostgresHook = PostgresHook.get_hook(Conn.POSTGRES)
+    processed_count = 0
 
     while True:
         records = pg_hook.get_records(_missing_caption_select_sql, parameters=[tree_type])
 
         if len(records) == 0:
-            return processed_abs_filenames
+            return CaptionStatus.NO_RECORDS_FOUND
 
         for _hash, preview, abs_filename, has_more_records in records:
             image_base64 = base64.b64encode(io.BytesIO(preview).read()).decode('utf-8')
             captions = _get_captions(caption_conf, image_base64)
             pg_hook.run(_caption_insert_sql, parameters=[_hash, caption_conf['caption_conf_id'], captions])
             pg_hook.run(_log_update_sql, parameters=[abs_filename])
-            processed_abs_filenames.append(abs_filename)
+            processed_count += 1
 
-            if not has_more_records or len(processed_abs_filenames) >= limit:
-                return processed_abs_filenames
+            if has_more_records:
+                if processed_count < limit:
+                    continue
+                else:
+                    return CaptionStatus.HAS_MORE_RECORDS
+            else:
+                return CaptionStatus.NO_RECORDS_LEFT
 
 
 def _get_captions(caption_conf, image_base64):
